031f0a79c66f1c0dc8c076e260dcc5ae1b3aafac
zhanghuan <1801210747@pku.edu.cn>
2020-07-14 22:50:31 +0800

    shared_ptr循环引用实例

--- part_8/circle.cc
+++ part_8/circle.cc
@@ -17,9 +17,16 @@ int main() {
     shared_ptr<A> pA = make_shared<A>();
     shared_ptr<B> pB = make_shared<B>();
     
+    cout << "pA的引用计数: " << pA.use_count() << endl; 
+    cout << "pB的引用计数: " << pB.use_count() << endl;
+
     pA->spa = pB;
-    pB->spb = pA;
+    // pB->spb = pA;
     
-    cout << pA.use_count() << " " << pB.use_count() << endl;
+    // 因为shared_ptr中的 引用计数 是 size_t* m_refcnt;
+    // pB的*m_refcnt本来就是1，现在pA来插一杠，pB的 *m_refcnt就是2了
+    cout << "pA的引用计数: " << pA.use_count() << endl; 
+    cout << "pB的引用计数: " << pB.use_count() << endl;
+    cout << pA->spa.use_count() << endl;
     return 0;
 }
