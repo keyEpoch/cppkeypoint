c40f0bdcea8f13f0ceca2d44b100882c351c2f2c
zhanghuan <1801210747@pku.edu.cn>
2020-07-18 20:46:13 +0800

    互斥锁应用在多线程访问临界变量

---
+++ part_10/main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+#define T_NUM 10    // 线程数
+
+using std::cout;
+using std::endl;
+
+int m = 0;
+std::mutex _mutex;
+
+void foo(std::string idx) {
+	std::lock_guard<std::mutex> lock(_mutex);
+	++m;
+	cout << "thread:" << idx.c_str() << ",  m:" << m  << endl;
+}
+
+int main() {
+	std::thread tarray[20];
+	for (int i = 0; i < T_NUM; ++i) {
+		tarray[i] = std::thread(foo, std::to_string(i));
+	}
+	for (int i = 0; i < T_NUM; ++i) {
+		tarray[i].join();
+	}
+
+	return 0;
+}
