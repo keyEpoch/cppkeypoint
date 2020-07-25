7c3ea58b349a7577f773f2255b06c534a1c6b7e5
zhanghuan <1801210747@pku.edu.cn>
2020-07-25 21:43:58 +0800

    C++

---
+++ part_12/test2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main() {
+	string s("zhang");
+	int num1 = 1, num2 = 2;
+
+	// thread t = thread([&](int a, int b) {     // 这样也可以
+	thread t = thread([num1, num2, s](int a, int b) {
+		cout << num1 + num2 + a + b << endl;
+		cout << s << endl;
+	}, 3, 4);
+	t.join();
+
+	return 0;
+}
